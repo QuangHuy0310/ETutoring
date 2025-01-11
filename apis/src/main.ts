33e6e2532123d175eac94dadeb020530be339219
QuangHuy0310 <huyng031003@gmail.com>
2025-01-11 09:15:58 +0700

    fear/auth

---
+++ apis/src/main.ts
@@ -0,0 +1,33 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory, Reflector } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { configs } from '@utils/configs/config';
+import { ResponseInterceptor } from '@utils/interceptors';
+
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+  });
+
+  app.useGlobalInterceptors(new ResponseInterceptor(app.get(Reflector)));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  const options = new DocumentBuilder()
+    .setTitle('BMA APIs')
+    .addBearerAuth()
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document);
+  await app.listen(configs.port);
+}
+bootstrap();
