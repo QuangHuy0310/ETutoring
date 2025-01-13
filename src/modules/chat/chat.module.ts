62f815279a34404d523d844baf1afd929b404dee
QuangHuy0310 <huyng031003@gmail.com>
2025-01-13 20:35:16 +0700

    fear/chat/test

---
+++ src/modules/chat/chat.module.ts
@@ -0,0 +1,22 @@
+import { ChatService } from './chat.service';
+import { ChatController } from './chat.controller';
+import { Module } from '@nestjs/common';
+import { ChatGateway } from './chat.gateway';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Message, MessageSchema } from '@entities/message.entities';
+
+@Module({
+    imports: [
+        MongooseModule.forFeature([
+            {
+              name: Message.name,
+              schema: MessageSchema,
+            },
+          ]),
+    ],
+    controllers: [
+        ChatController,],
+    providers: [
+        ChatService, ChatGateway],
+})
+export class ChatModule { }
--- src/modules/index-service.ts
+++ src/modules/index-service.ts
@@ -1,2 +1,4 @@
 export * from '@modules/auth/auth.service';
 export * from '@modules/user/user.service';
+export * from '@modules/chat/chat.service';
+
