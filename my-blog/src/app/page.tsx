f17bd6d90e777296be55b769295ae1528117b198
lap22 <mainguyenlap22@gmail.com>
2025-02-11 15:16:04 +0700

    FE

--- my-blog/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import PostForm from "@/components/Postform";
-
-export default function Home() {
-  return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <PostForm />
-    </div>
-  );
-}
