a0b2c954fe1fcd41a6ede3cae9d7e1e5bfd52b7a
Nguyen Hoang (FGW HCM) <hoangngcs210919@hoangngcs210919@fpt.edu.vn>
2025-02-25 16:33:00 +0700

    fix/ authorization

--- apis/src/modules/specialUser/specialUser.controller.ts
+++ apis/src/modules/specialUser/specialUser.controller.ts
@@ -1,11 +1,14 @@
 import { Controller, Post, Body, BadRequestException } from '@nestjs/common';
 import { SpecialUserService } from './specialUser.service';
 import { SpecialUserDto } from '@dtos/specialUser.dto';
+import { RequiredByUserRoles } from '@utils/decorator';
+import { USER_ROLE } from '@utils/data-types/enums';
 
-@Controller('api/v1/admin/special')
+@Controller()
 export class SpecialUserController {
   constructor(private readonly specialUserService: SpecialUserService) {}
 
+  @RequiredByUserRoles(USER_ROLE.ADMIN)
   @Post('add')
   async createSpecialUser(@Body() createSpecialUserDto: SpecialUserDto) {
     try {
