831c812175e79868720fb8c40552528e6b417f28
❥KiyoshiდKunツ <kiyoshikuncool@gmail.com>
2025-03-19 23:36:20 +0700

    jztr

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,5 @@
-import { MajoringModule } from './modules/majoring/majoring.module';
 import { AuthenticationGuard } from '@guards/authentication.guard';
 import * as MODULES from '@modules';
-import { MailerModule } from '@nestjs-modules/mailer';
 import { Module } from '@nestjs/common';
 import { APP_FILTER, APP_GUARD, RouterModule } from '@nestjs/core';
 import { JwtModule } from '@nestjs/jwt';
