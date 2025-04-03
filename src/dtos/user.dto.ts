dede4be6ce24be27c372f374538d28e4f820b1df
Nguyen Hoang (FGW HCM) <hoangngcs210919@hoangngcs210919@fpt.edu.vn>
2025-04-03 20:28:32 +0700

    edit use body

--- src/dtos/user.dto.ts
+++ src/dtos/user.dto.ts
@@ -1,5 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsEnum, IsOptional } from 'class-validator';
+import { USER_ROLE } from '@utils/data-types/enums';
+
 export class CreateNewUserDto {
   email: string;
   hash: string;
   role: string;
+}
+
+export class UpdateUserDto {
+  @ApiProperty({ description: 'The email of the user', example: 'newemail@example.com' })
+  @IsEmail({}, { message: 'Invalid email format' })
+  @IsOptional()
+  email: string;
+
+  @ApiProperty({ description: 'The role of the user', enum: USER_ROLE, example: 'ADMIN' })
+  @IsEnum(USER_ROLE, { message: 'Invalid role' })
+  @IsOptional()
+  role: USER_ROLE;
 }
--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,9 +1,10 @@
 import { BadRequestException, Body, Controller, Get, Post, Request, UploadedFile, UseInterceptors, Delete, Put, Query } from '@nestjs/common';
-import { ApiTags, ApiQuery, ApiOperation } from '@nestjs/swagger';
+import { ApiTags, ApiQuery, ApiOperation, ApiBody } from '@nestjs/swagger';
 import { AuthorizationRequest } from '@utils/data-types/types';
 import { RequiredByUserRoles } from '@utils/decorator';
 import { UserService } from './user.service';
 import { RegisterDto } from '@dtos/auth.dto';
+import { UpdateUserDto } from '@dtos/user.dto'; // Import DTO mới
 import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import { USER_ROLE } from '@utils/data-types/enums';
@@ -51,7 +52,6 @@ export class UserController {
     };
   }
 
-  // API: Get all users
   @ApiOperation({ summary: 'Get all users' })
   @RequiredByUserRoles(USER_ROLE.ADMIN)
   @Get('/get-all-users')
@@ -59,7 +59,6 @@ export class UserController {
     return this.userService.getAllUsers();
   }
 
-  // API: Delete a user by ID
   @ApiQuery({ name: 'id', required: true })
   @ApiOperation({ summary: 'Delete a user by ID' })
   @RequiredByUserRoles(USER_ROLE.ADMIN)
@@ -69,14 +68,14 @@ export class UserController {
     return { message: 'User deleted successfully' };
   }
 
-  // API: Update a user by ID
   @ApiQuery({ name: 'id', required: true })
   @ApiOperation({ summary: 'Update a user by ID' })
+  @ApiBody({ type: UpdateUserDto }) 
   @RequiredByUserRoles(USER_ROLE.ADMIN)
   @Put('/edit-user')
   async updateUser(
     @Query('id') id: string,
-    @Body() body: { email: string, role: USER_ROLE },
+    @Body() body: UpdateUserDto, 
   ) {
     return this.userService.updateUser(id, body.email, body.role);
   }
