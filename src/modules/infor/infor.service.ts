fee153df22ce72830e22acf1e10673487d00412f
Nguyen Hoang (FGW HCM) <hoangngcs210919@hoangngcs210919@fpt.edu.vn>
2025-05-02 00:23:38 +0700

    fix edit infor

--- src/modules/infor/infor.service.ts
+++ src/modules/infor/infor.service.ts
@@ -172,6 +172,7 @@ export class InforService {
         information.address = payload.address;
         information.major = payload.major;
         information.country = payload.country;
+        information.path = payload.path; 
 
         const updatedInformation = await information.save();
 
